8bf5b7f135874972c33ffc83c30a2688af86fd53
lijjsk <2609478776@qq.com>
2023-11-17 11:11:38 +0800

    Initial commit

---
+++ lijjsk-wemedia-model/src/main/java/com/lijjsk/model/wmuser/dtos/WmUserDto.java
@@ -0,0 +1,4 @@
+package com.lijjsk.model.wmuser.dtos;
+
+public class WmUserDto {
+}
---
+++ lijjsk-wemedia-service/lijjsk-wemedia-login/src/main/java/com/lijjsk/login/LoginApplication.java
@@ -0,0 +1,14 @@
+package com.lijjsk.login;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+
+@SpringBootApplication
+@EnableDiscoveryClient
+public class LoginApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(LoginApplication.class,args);
+    }
+}
