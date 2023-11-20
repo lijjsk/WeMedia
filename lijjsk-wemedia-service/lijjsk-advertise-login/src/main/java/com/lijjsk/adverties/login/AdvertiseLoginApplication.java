f2ad779466d83ad1a979e7c15136b90a1c2cd835
lijjsk <2609478776@qq.com>
2023-11-20 17:06:55 +0800

    2023/11/19 16:46

---
+++ lijjsk-wemedia-service/lijjsk-advertise-login/src/main/java/com/lijjsk/adverties/login/AdvertiseLoginApplication.java
@@ -0,0 +1,11 @@
+package com.lijjsk.adverties.login;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdvertiseLoginApplication {
+    public static void main(String []args){
+        SpringApplication.run(AdvertiseLoginApplication.class,args);
+    }
+}
---
+++ lijjsk-wemedia-service/lijjsk-manage-login/src/main/java/com/lijjsk/manage/login/ManageLoginApplication.java
@@ -0,0 +1,11 @@
+package com.lijjsk.manage.login;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ManageLoginApplication {
+    public static void main(String []args){
+        SpringApplication.run(ManageLoginApplication.class,args);
+    }
+}
